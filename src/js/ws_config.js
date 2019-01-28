6472d69ab988012817e9e2a600924e6f5b1bc631
crypto117 <46241441+crypto117@users.noreply.github.com>
2019-01-27 23:09:16 -0800

    Update ws_config.js

--- src/js/ws_config.js
+++ src/js/ws_config.js
@@ -2,63 +2,63 @@ var config = {};
 
 // self explanatory, your application name, descriptions, etc
 config.appName = 'WalletShell';
-config.appDescription = 'TurtleCoin Wallet';
+config.appDescription = 'RhodiumCoin Wallet';
 config.appSlogan = 'Slow and steady wins the race!';
-config.appId = 'lol.turtlecoin.walletshell';
-config.appGitRepo = 'https://github.com/turtlecoin/turtle-wallet-electron';
+config.appId = 'lol.rhodiumcoin.walletshell';
+config.appGitRepo = 'https://github.com/rhodiumcoin/rhodium-wallet-electron';
 
-// default port number for your daemon (e.g. TurtleCoind)
-config.daemonDefaultRpcPort = 11898;
+// default port number for your daemon (e.g. RhodiumCoind)
+config.daemonDefaultRpcPort = 8004;
 
 // wallet file created by this app will have this extension
 config.walletFileDefaultExt = 'twl';
 
 // change this to match your wallet service executable filename
-config.walletServiceBinaryFilename = 'turtle-service';
+config.walletServiceBinaryFilename = 'rhodium-service';
 
 // version on the bundled service (turtle-service)
 config.walletServiceBinaryVersion = "v0.12.0";
 
 // config file format supported by wallet service, possible values:
-// ini -->  for turtle service (or its forks) version <= v0.8.3
-// json --> for turtle service (or its forks) version >= v0.8.4
+// ini -->  for rhodium service (or its forks) version <= v0.8.3
+// json --> for rhodium service (or its forks) version >= v0.8.4
 config.walletServiceConfigFormat = "json";
 
-// default port number for your wallet service (e.g. turtle-service)
+// default port number for your wallet service (e.g. rhodium-service)
 config.walletServiceRpcPort = 8070;
 
 // block explorer url, the [[TX_HASH]] will be substituted w/ actual transaction hash
-config.blockExplorerUrl = 'https://explorer.turtlecoin.lol/transaction.html?hash=[[TX_HASH]]';
+config.blockExplorerUrl = null;
 
 // default remote node to connect to, set this to a known reliable node for 'just works' user experience
-config.remoteNodeDefaultHost = 'turtlenode.co';
+config.remoteNodeDefaultHost = 'rhodiumnode.co';
 
 // remote node list update url, set to null if you don't have one
-config.remoteNodeListUpdateUrl = 'https://raw.githubusercontent.com/turtlecoin/turtlecoin-nodes-json/master/turtlecoin-nodes.json';
+config.remoteNodeListUpdateUrl = null;
 
 // fallback remote node list, in case fetching update failed, fill this with known to works remote nodes
 config.remoteNodeListFallback = [
-  'turtlenode.co:11898',
-  'nodes.hashvault.pro:11898',
-  'public.turtlenode.io:11898',
-  'public.turtlenode.net:11898',
+  'rhodiumnode.co:8004',
+  'nodes.hashvault.pro:8004',
+  'public.rhodiumnode.io:8004',
+  'public.rhodiumnode.net:8004',
 ];
 
 // your currency name
-config.assetName = 'TurtleCoin';
+config.assetName = 'RhodiumCoin';
 // your currency ticker
-config.assetTicker = 'TRTL';
+config.assetTicker = 'RHOX';
 // your currency address prefix, for address validation
-config.addressPrefix = 'TRTL';
+config.addressPrefix = 'Rhod';
 // standard wallet address length, for address validation
 config.addressLength = 99;
 // intergrated wallet address length, for address validation
 config.integratedAddressLength = 187;
 
 // minimum fee for sending transaction
-config.minimumFee = 0.1;
+config.minimumFee = 10;
 // minimum amount for sending transaction
-config.mininumSend = 0.1;
+config.mininumSend = 1;
 // default mixin/anonimity for transaction
 config.defaultMixin = 3;
 // to convert from atomic unit
